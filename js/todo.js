cb7d3be8dc56526f6cd1130b089057fdf2ed1ab6
yeji seo <yejiseo0725@gmail.com>
2022-02-01 17:40:26 +0900

    todo_list and delete

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,37 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = 
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const liDelete = event.target.parentElement;
+  liDelete.remove();
+}
+
+function paintToDo(newTodo) {
+  const liPaint = document.createElement("li");
+  const spanPaint = document.createElement("span");
+  spanPaint.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  liPaint.appendChild(spanPaint); // li 는 span 이라는 자식을 가지게 된다.
+  liPaint.appendChild(button);
+  toDoList.appendChild(liPaint);
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newTodo);
+  paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
